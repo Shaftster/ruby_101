51fa0053756aaf4306cac373c8105f0db50c8707
Jermaine Wates <jwates7@protonmail.com>
2023-02-08 22:12:14 -0700

    add puts

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,11 @@
 array_1 = ['cats', 'dogs', 'cattle', 'swine' ]
 array_2 = [1982, 1978, 1984, 1987]
 array_3 = [98.6, 101.2, 40.1, 26.3]
-arry_4 = [true, true, false, true, false]
+array_4 = [true, true, false, true, false]
+
+   
+
+   puts array_2.unshift
+   puts array_4.shift
+   puts array_1.pop
+   puts array_3.push
