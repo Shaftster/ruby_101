0853e2d9a55bd60eaa9a3a34873496bf19f545b1
Jermaine Wates <jwates7@protonmail.com>
2023-02-08 22:18:01 -0700

    add indexes

--- arrays.rb
+++ arrays.rb
@@ -9,3 +9,13 @@ array_4 = [true, true, false, true, false]
    puts array_4.shift
    puts array_1.pop
    puts array_3.push
+   
+   
+   puts array_3[3]
+   # index to find 26.3 at the third position in array_3
+   puts array_4[0]
+   # index to find boolean 'true' in array_4
+   puts array_2[2]
+   # index to find integer '1984 in array_2
+   puts array_1[1]
+   # index to find 'dogs' in array_1
